0152b00eda3041b665aad93b7c47defa482aea7d
Benson Kiai <benson.kiai86@gmail.com>
2023-10-05 23:19:58 +0300

    Update class to resolve bugs

--- class_person.rb
+++ class_person.rb
@@ -1,12 +1,26 @@
-class Person < Nameable
-  attr_accessor :name
+require_relative 'class_nameable'
 
-  def initialize(name)
+class Person < Nameable
+  attr_accessor :name, :age
+
+  def initialize(age, name, parent_permission: true)
     super()
     @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
 
   def correct_name
     @name
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
