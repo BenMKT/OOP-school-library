a959cb5f24eafc6cbffae37f0930e48aeacb9963
Benson Kiai <benson.kiai86@gmail.com>
2023-10-06 01:24:35 +0300

    Update methods

--- class_student.rb
+++ class_student.rb
@@ -6,10 +6,19 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    return if @classroom == classroom
+
+    @classroom&.students&.delete(self)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
 
