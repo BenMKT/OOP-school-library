2b8d31e9dc71d30867db02bab71a0574dc5ae1f5
Benson Kiai <benson.kiai86@gmail.com>
2023-10-06 01:23:44 +0300

    Refactor to update variables and methods

--- class_person.rb
+++ class_person.rb
@@ -2,12 +2,16 @@ require_relative 'nameable'
 
 class Person < Nameable
   attr_accessor :name, :age
+  attr_reader :id, :rentals
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+
+  def initialize(age, rentals, name = 'Unknown', parent_permission: true)
     super()
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
@@ -16,6 +20,12 @@ class Person < Nameable
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def add_rental(book, date)
+    rental = Rental.new(date, book, self)
+    @rentals.push(rental)
+    rental
   end
 
   private
