9f5d29159acdd452a45fef8cb9981aa8debcba6a
Benson Kiai <benson.kiai86@gmail.com>
2023-10-06 07:42:50 +0300

    Refactor add_rental method

--- class_book.rb
+++ class_book.rb
@@ -8,8 +8,6 @@ class Book
   end
 
   def add_rental(person, date)
-    rental = Rental.new(date, self, person)
-    @rentals.push(rental)
-    rental
+    Rental.new(date, self, person)
   end
 end
