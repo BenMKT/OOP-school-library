4502e727dad10172973232d77f63014e2ac6cbe9
Benson Kiai <benson.kiai86@gmail.com>
2023-10-07 11:42:58 +0300

    Refactor main.rb to resolve bug

--- main.rb
+++ main.rb
@@ -1,8 +1,46 @@
 require_relative 'app'
 
+# This method prints the options the user can choose from and gets the user input
+def display_options
+    puts 'Options:'
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List rentals for a person'
+    puts '7. Exit'
+  end
+
+# This method is the main method that will run and exit the app
 def main
   app = App.new
-  app.run
+
+  loop do
+    display_options
+    option = gets.chomp.to_i
+    if option == 7
+      puts 'Thank you for using the app. Goodbye!'
+      break
+    elsif option.between?(1, 6)
+      case option
+      when 1
+        app.list_books
+      when 2
+        app.list_people
+      when 3
+        app.create_person
+      when 4
+        app.create_book
+      when 5
+        app.create_rental
+      when 6
+        app.list_rentals_for_person
+      end
+    else
+      puts 'Error: Invalid number, try again'
+    end
+  end
 end
 
-main if __FILE__ == $PROGRAM_NAME
+main
